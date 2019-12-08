793608d6691920bbc4902dc3dfd84d130d523659
ingesolvoll <inge@semion.ca>
2019-12-08 21:02:39 +0100

    Release 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kee-frame-sockets "0.0.1-SNAPSHOT"
+(defproject kee-frame-sockets "0.0.1"
   :description "Websocket support for kee-frame"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
